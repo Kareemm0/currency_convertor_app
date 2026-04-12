f9f8ec71d4fb3d1aea5ec028f78461a4c9fd6fc8
kareem mohamed <kareemmohamed@MacBook-Air-kareem.local>
2026-04-12 22:28:10 +0200

    feat: add injection container

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:currency_convertor_app/app.dart';
+import 'package:currency_convertor_app/injection_container.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
   runApp(const MyApp());
 }
