d22b0a43427d028786d417d6896ac8a8dbb47088
kareem mohamed <kareemmohamed@MacBook-Air-kareem.local>
2026-04-13 01:55:07 +0200

    feat: apply convert

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:currency_convertor_app/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'presentation/presentation.dart';
 
@@ -7,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (context) => CurrencyCodeCubit(repo: getIt())..getRates(),
+        child: HomeScreen(),
+      ),
+    );
   }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:currency_convertor_app/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../core/core.dart';
@@ -14,97 +13,115 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String? qoutesValue;
   String? baseValue;
+  bool isShwoen = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => CurrencyCodeCubit(repo: getIt())..getRates(),
-        child: BlocBuilder<CurrencyCodeCubit, CurrencyCodeState>(
-          builder: (ctx, state) {
-            return switch (state) {
-              CurrencyCodeLoadingState() => Center(
-                child: CircularProgressIndicator(),
-              ),
-              CurrencyCodeFailurState(msg: final msg) => Center(
-                child: Text(msg),
-              ),
-              CurrencyCodeSuccsseState(
-                currencyConvertorModel: final currencyConvertorModel,
-              ) =>
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 40,
-                  ),
-                  child: Column(
-                    spacing: 16,
-                    mainAxisAlignment: .center,
-                    children: [
+      body: BlocConsumer<CurrencyCodeCubit, CurrencyCodeState>(
+        listener: (context, state) {
+          if (state is ConvertResultSuccessState) {
+            isShwoen = true;
+          }
+        },
+        builder: (ctx, state) {
+          return switch (state) {
+            CurrencyCodeLoadingState() => Center(
+              child: CircularProgressIndicator(),
+            ),
+            CurrencyCodeFailurState(msg: final msg) => Center(child: Text(msg)),
+            CurrencyCodeSuccsseState(
+              currencyConvertorModel: final currencyConvertorModel,
+            ) =>
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 40,
+                ),
+                child: Column(
+                  spacing: 16,
+                  mainAxisAlignment: .center,
+                  children: [
+                    Text(
+                      "Currency Convertor ",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.textColor,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.containerColor,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: CurrencyConvertorWidget(
+                        imageUrl: '',
+                        items: currencyConvertorModel
+                            .map(
+                              (e) => DropdownMenuItem(
+                                value: e.quote,
+                                child: Text(e.quote ?? ""),
+                              ),
+                            )
+                            .toList(),
+                        onQoutesChanged: (val) {
+                          setState(() {
+                            qoutesValue = val;
+                          });
+                        },
+                        onBaseChanged: (val) {
+                          setState(() {
+                            baseValue = val;
+                          });
+                        },
+                        baseValue:
+                            baseValue ?? currencyConvertorModel.first.quote,
+                        qoutesValue:
+                            qoutesValue ?? currencyConvertorModel.first.quote,
+                      ),
+                    ),
+
+                    if (state is ConvertResultSuccessState)
                       Text(
-                        "Currency Convertor ",
+                        "1 $baseValue = ${state.rateValue} $qoutesValue",
                         style: TextStyle(
-                          fontSize: 26,
-                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
                           color: AppColors.textColor,
                         ),
                       ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 20,
-                        ),
-                        decoration: BoxDecoration(
-                          color: AppColors.containerColor,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: CurrencyConvertorWidget(
-                          imageUrl: '',
-                          items: currencyConvertorModel
-                              .map(
-                                (e) => DropdownMenuItem(
-                                  value: e.quote,
-                                  child: Text(e.quote ?? ""),
-                                ),
-                              )
-                              .toList(),
-                          onQoutesChanged: (val) {
-                            setState(() {
-                              qoutesValue = val;
-                            });
-                          },
-                          onBaseChanged: (val) {
-                            setState(() {
-                              baseValue = val;
-                            });
-                          },
-                          baseValue:
-                              baseValue ?? currencyConvertorModel.first.quote,
-                          qoutesValue:
-                              qoutesValue ?? currencyConvertorModel.first.quote,
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.textColor,
-                          minimumSize: Size(double.maxFinite, 50),
-                        ),
-                        child: Text(
-                          "Convert".toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: AppColors.white,
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<CurrencyCodeCubit>().getConvertResult(
+                          inputs: RatesInputs(
+                            base: baseValue ?? "",
+                            qoutes: qoutesValue ?? "",
                           ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.textColor,
+                        minimumSize: Size(double.maxFinite, 50),
+                      ),
+                      child: Text(
+                        "Convert".toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.white,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              _ => SizedBox.shrink(),
-            };
-          },
-        ),
+              ),
+            _ => SizedBox.shrink(),
+          };
+        },
       ),
     );
   }
